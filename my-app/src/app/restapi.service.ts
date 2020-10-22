1a8f133413873a3d312993e5462bc74fc012e5fd
reymesson1 <reymesson1>
2020-10-22 12:02:32 -0400

    done

--- my-app/src/app/restapi.service.ts
+++ my-app/src/app/restapi.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'; 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import {Router} from '@angular/router';
